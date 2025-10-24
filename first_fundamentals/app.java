1fe75f90beb6942dd5d79f00ae74473fdc1fea16
althaf964 <tappaalthaf6469@gmail.com>
2025-10-24 21:11:32 +0530

    1stJava

---
+++ first_fundamentals/app.java
@@ -0,0 +1,10 @@
+public class app {
+    
+    public static void main(String[] args) {
+        System.out.println( "Welcome to Java Course");
+       int a = 10;
+       String name = "Althaf";
+       System.out.println(name);
+       System.out.println(a);
+    }
+}
