a8574c80480febb3449b73343db58648160672fe
althaf964 <tappaalthaf6469@gmail.com>
2025-11-04 15:29:10 +0530

    CarDetails.java

--- second_variables/CarDetails.java
+++ second_variables/CarDetails.java
@@ -1,5 +1,29 @@
 package second_variables;
 
 public class CarDetails {
+    public static void main(String[] args) {
+        String name = "Tata Punch";
+        String Engine = "1199 cc";
+        String power = "72-87 bh";
+        String Transmission = "Manual";
+        byte Rating = 5;
+        String Groundclearence = "187 mm";
+        String Torque = "103 Nm- 115Nm";
+        String DriveType = "FWD";
+        byte SeatCapacity = 5;
+        byte NoOfCylinders = 4;
+
+        System.out.println("Vehicle Specifications:");
+        System.out.println("CarName:" +name);
+        System.out.println("Engine Type:" +Engine);
+        System.out.println("Power:" +power);
+        System.out.println("Transmission:" +Transmission);
+        System.out.println("Gloabal Rating:" +Rating);
+        System.out.println("GroundClearence:" +Groundclearence);
+        System.out.println("Torque:" +Torque);
+        System.out.println("Driving Type:" +DriveType);
+        System.out.println("Saet capacity:" +SeatCapacity);
+        System.out.println("NoofCylinders:"+NoOfCylinders);
+    }
 
 }
